848799d41df5d5ad18d1970c13eff65e785c38e3
Gustavo Victor <ferreirag930@yahoo.com>
2025-04-25 11:49:33 -0300

    ex10

--- array10.js
+++ array10.js
@@ -1,7 +1,8 @@
 let jogos = ["Borderlands 2", "Payday 2", "Minecraft"];
-let jogoFavorito = "Borderlands 2";
+let jogoFavorito = "Payday 2";
+let quantidade = jogos.length;
 
-for(let i = 0; i < 1 ; i++){
+for(let i = 0; i < quantidade ; i++){
     if(jogoFavorito === jogos[i]){
         console.log(`Meu jogo favorito é ${jogos[i]} está no ranking na posição ${i+1}`);
     }
