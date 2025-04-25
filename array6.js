03cc5a9408faa4826fbcd99b056e26f07230227d
Gustavo Victor <ferreirag930@yahoo.com>
2025-04-25 10:21:12 -0300

    ex7

---
+++ array6.js
@@ -0,0 +1,6 @@
+let filmes = ["Blade Runner", "Vertigo", "Persona", "Portrait de la jeune fille en feu"];
+let quantidade = filmes.length;
+
+for(let i = quantidade -1; i > -1; i--){
+    console.log(filmes[i]);
+}
