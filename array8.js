9985c292fb75d917699a3feabafbf38fefc75305
Gustavo Victor <ferreirag930@yahoo.com>
2025-04-25 10:39:22 -0300

    ex8

---
+++ array8.js
@@ -0,0 +1,8 @@
+let idades = ["8", "12", "17", "21", "22"];
+let quantidade = idades.length;
+
+for(let i = 0; i < quantidade; i++){
+    if(idades[i] >= 18){
+        console.log(idades[i]);
+    }
+}
