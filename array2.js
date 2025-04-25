7fabbad2cc138ee02fa6af771186277d4d1b645a
Gustavo Victor <ferreirag930@yahoo.com>
2025-04-25 09:32:09 -0300

    ex2

---
+++ array2.js
@@ -0,0 +1,6 @@
+let playlist = ["Careful With That Axe, Eugene", "Echoes", "A Saucerful of Secrets", "Set the Controls for the Heart of the Sun"];
+let quantidade = playlist.length;
+
+for(let i = 0; i < quantidade; i++){
+    console.log(playlist[i]);
+}
